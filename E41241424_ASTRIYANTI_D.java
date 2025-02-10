5b98cf14dad73b3c0917d038bcdbfef9ba20dc40
astriyanti-p <astreedpip@gmail.com>
2025-02-10 20:27:09 +0700

    Create E41241424_ASTRIYANTI_D.java

---
+++ E41241424_ASTRIYANTI_D.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.e41241424_astriyanti_d;
+
+/**
+ *
+ * @author Astrid
+ */
+public class E41241424_ASTRIYANTI_D {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
