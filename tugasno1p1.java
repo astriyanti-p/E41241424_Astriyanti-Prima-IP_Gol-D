2b3439e3a66c64166f4062677aa1e0f0d1156d69
astriyanti-p <astreedpip@gmail.com>
2025-02-13 21:45:23 +0700

    Create tugasno1p1.java

---
+++ tugasno1p1.java
@@ -0,0 +1,26 @@
+package com.mycompany.minggu2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Astrid
+ */
+public class tugasno1p1 {
+    public static void main(String[] args) {
+        String fName, IName, stuId, stuStatus;
+        
+        fName = "Lisa Palombo";
+        stuId = "123456789";
+        stuStatus = "Active";
+        
+        System.out.println("Student Name : " +fName);
+        System.out.println("Student Id : " +stuId);
+        System.out.println("Student Status ; " +stuStatus);
+     
+    }
+    
+}
