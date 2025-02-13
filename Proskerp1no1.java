dca2fde245d124b204827c216f7315eb084049bd
astriyanti-p <astreedpip@gmail.com>
2025-02-13 21:44:09 +0700

    Create Proskerp1no1.java

---
+++ Proskerp1no1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minggu2;
+
+/**
+ *
+ * @author Astrid
+ */
+public class Proskerp1no1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("World!");   
+        
+        System.out.print("\nHello!");
+        System.out.print("World!");
+    }
+}
