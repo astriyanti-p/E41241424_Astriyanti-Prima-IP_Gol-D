179e592d4c24df850a3c6895593d5ad115cc064c
astriyanti-p <astreedpip@gmail.com>
2025-02-13 21:44:21 +0700

    Create Proskerp1no2.java

---
+++ Proskerp1no2.java
@@ -0,0 +1,24 @@
+package com.mycompany.minggu2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Astrid
+ */
+public class Proskerp1no2 {
+    public static void main(String[] args) {
+        String nama = "Indonesia";
+         char abjad ='a';
+         int a = 6;
+         double b = 6.9;
+         
+          System.out.println(nama);
+          System.out.println(a);
+         
+    }
+    
+}
